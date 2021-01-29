66dd6b601c1f6a1bfaab5c890003fd758a297d93
Your Name <you@example.com>
2021-01-29 13:41:33 +0200

    basic app

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+
+//routes
+const locations = require("./routes/locations.js");
+
+//middleware
+app.use("/locations", locations);
+
+locations.get("/", (req, res) => {
+  res.send("hello");
+});
+
+locations.get("/1", (req, res) => {
+  res.send({"id": 1, "latitude": 60, "longitude": 60});
+});
+
+app.listen(port, () => {
+  console.log("listening to port");
+});
